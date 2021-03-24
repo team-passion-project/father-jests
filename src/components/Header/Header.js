7f2ded3559cd9c024c7a94c8791320d6fb768e4a
chrisjaechun <chrisjaechun@gmail.com>
2021-03-24 11:58:42 -0400

    Add Header and basic styling
    - Added Header with logo and added background color
    - Installed SASS and react-bootstrap packages

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+
+const Header = () =>
+  <Navbar fixed="top" className="navbar-header">
+    <Navbar.Brand>
+      <div className="logo">
+        FATHER JESTS
+      </div>
+    </Navbar.Brand>
+  </Navbar>
+
+export default Header
