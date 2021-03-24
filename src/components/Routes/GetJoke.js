fc351a35605c6b436f1c685395100dfc7e6f55ae
hankd11234 <hankd11234@gmail.com>
2021-03-24 14:47:59 -0400

    Add button to change joke

--- src/components/Routes/GetJoke.js
+++ src/components/Routes/GetJoke.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import Button from 'react-bootstrap/Button'
+import button from 'react-bootstrap/Button'
 
 function GetJoke() {
   const [joke, newJoke] = useState("")
@@ -18,9 +18,21 @@ function GetJoke() {
   }, [])
 
   return (
-    <div className="jokes">
+    <div className="joke">
       <h2>{joke}</h2>
-      <button type="button" class="btn btn-danger" onClick={getJoke}>Get a new Joke</button>
+      <button type="button" class="btn btn-danger" onClick={() => 
+        {
+          const getJoke = async() => {
+            const result = await axios({
+              url: "https://icanhazdadjoke.com/",
+              headers: {Accept: "application/json"}
+            })
+            console.log(result.data);
+            newJoke(`${result.data.joke}`)
+          }
+          getJoke()
+        }
+      }>Get a new Joke</button>
     </div>
   )
 }
